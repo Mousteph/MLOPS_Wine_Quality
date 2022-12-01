23642c364e22167e2d8db2c8e864ee19fcfbac6c
Moustapha DIOP <moustephdiop@gmail.com>
2022-12-01 10:07:22 +0100

    (fix) error connecting to postgres

--- src/save_data/src/postgresql/database.py
+++ src/save_data/src/postgresql/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import time
 
 USER = os.environ.get("POSTGRES_USER")
 PASSWORD = os.environ.get("POSTGRES_PASSWORD")
@@ -10,7 +11,21 @@ URL = os.environ.get("POSTGRES_URL")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{URL}/{DATABASE}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+repeat = 10
+is_ok = False
+e_msg = None
 
-Base = declarative_base()
+while not is_ok and repeat != 0:
+    try: 
+        engine = create_engine(SQLALCHEMY_DATABASE_URL)
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        Base = declarative_base()
+
+        is_ok = True
+    except Exception as e:
+        e_msg = e
+        time.sleep(4)
+        repeat -= 1
+        
+if repeat == 0:
+    raise RuntimeError(f"Cannot connect to Postgresql: {e_msg}")
