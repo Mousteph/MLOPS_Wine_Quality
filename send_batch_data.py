3209bdb89188ba2fad8136b6959a144678f1fcb6
Moustapha DIOP <moustephdiop@gmail.com>
2022-12-01 19:30:10 +0100

    (update) gitignore

---
+++ send_batch_data.py
@@ -0,0 +1,44 @@
+import sys
+
+import requests
+import pandas as pd
+import numpy as np
+
+def get_bad_data(path):
+    data = pd.read_pickle(path).drop(columns=['quality'])
+    data["volatile acidity"] = 3 + data["volatile acidity"]
+    data["citric acid"] = 5 + data["citric acid"]
+    data["alcohol"] = 20
+
+    return data.values
+
+def get_good_data(path):
+    data = pd.read_pickle(path).drop(columns=['quality'])
+    return data.values
+
+def get_noisy_data(path):
+    data = pd.read_pickle(path).drop(columns=['quality'])
+    data = data + np.random.normal(0.1, 0.01, data.shape) 
+    return data.values
+
+def send_data(data):
+    json = {"data": data.tolist()}
+    requests.post("http://localhost:80/batch", json=json)
+
+if __name__ == '__main__':
+    arg = sys.argv
+    
+    if len(arg) != 2:
+        exit(0)
+    
+    path = "./data/training/wine.pkl"
+    if arg[1] == "corrupted":
+        data = get_bad_data(path)
+    
+    elif arg[1] == "normal":
+        data = get_good_data(path)
+        
+    elif arg[1] == "noisy":
+        data = get_noisy_data(path)
+    
+    send_data(data)
