4e24dee52cb332ac2eb3295a21dc6c5a75713ce6
Moustapha DIOP <moustephdiop@gmail.com>
2023-10-18 16:56:44 +0200

    (update) doc frontend datadrift

--- src/frontend_data_drift/src/main_frontend_drift.py
+++ src/frontend_data_drift/src/main_frontend_drift.py
@@ -3,6 +3,7 @@ import streamlit as st
 import sender
 import pandas as pd
 import json
+from typing import Tuple
 
 st.set_page_config(
     page_title="Red Wine Data Drift",
@@ -20,7 +21,16 @@ last_AUC = None
 date = None
 chart_data = None
 
-def get_drift(new=False):
+def get_drift(new:bool = False) -> pd.DataFrame:
+    """Get data drift from backend
+
+    Args:
+        new (bool, optional): If True, get new data drift. Defaults to False.
+
+    Returns:
+        pd.DataFrame: DataFrame of data drift
+    """
+    
     if new:
         y = sender.new_drift()
     else:
@@ -33,7 +43,16 @@ def get_drift(new=False):
         d = json.loads(y["data"])
         return pd.DataFrame(d).drop(columns=["Unnamed: 0"])
     
-def update_auc(data):
+def update_auc(data: pd.DataFrame) -> Tuple:
+    """Update AUC and date from data drift
+
+    Args:
+        data (pd.DataFrame): DataFrame of data drift
+
+    Returns:
+        Tuple: Tuple of (date, AUC)
+    """
+    
     if data is not None and len(data) != 0:
         last = data.iloc[-1]
         return last.iloc[0], last.iloc[1]
